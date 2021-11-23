6f566b25b5ffb8c70fdbe54bc2ed4efaff22e316
Jo Jo Lam <jojilam@gmail.com>
2021-11-23 15:23:18 -0800

    finished migration

---
+++ SpeakerAPI/Data/SpeakerDbContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SpeakerLibrary;
+
+namespace SpeakerAPI.Data
+{
+    public class SpeakerDbContext : DbContext
+    {
+        public DbSet<Speaker> Students { get; set; }
+
+        public SpeakerDbContext(DbContextOptions<SpeakerDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Speaker>().HasData(
+              new
+              {
+                  SpeakerId = Guid.NewGuid().ToString(),
+                  FirstName = "Tom",
+                  LastName = "Day",
+                  Email = "tom@day.com",
+                  Mobile = "6041111111",
+                  Specialization = "Love",
+                  City = "Burnaby",
+                  Province = "BC",
+                  Employer = "Tinder"
+              }, new
+              {
+                  SpeakerId = Guid.NewGuid().ToString(),
+                  FirstName = "Ann",
+                  LastName = "Fox",
+                  Email = "ann@fox.com",
+                  Mobile ="6042222222",
+                  Specialization ="Hunting",
+                  City ="Langley",
+                  Province ="BC",
+                  Employer ="Hunter"
+              }, new
+              {
+                  SpeakerId = Guid.NewGuid().ToString(),
+                  FirstName = "Art",
+                  LastName = "Ash",
+                  Email = "art@ash.com",
+                  Mobile = "6043333333",
+                  Specialization = "Art",
+                  City = "Vancouver",
+                  Province = "BC",
+                  Employer = "Art Gallery"
+              }, new
+              {
+                  SpeakerId = Guid.NewGuid().ToString(),
+                  FirstName = "Mia",
+                  LastName = "Hay",
+                  Email = "mia@hay.com",
+                  Mobile = "6044444444",
+                  Specialization = "Farmer",
+                  City = "Coquitlam",
+                  Province = "BC",
+                  Employer = "Hayers"
+              }
+            );
+        }
+    }
+
+}
---
+++ SpeakerLibrary/Speaker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpeakerLibrary
+{
+    public class Speaker
+    {
+        public string SpeakerId { get; set; }
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name ="Mobile Number")]
+        public string Mobile { get; set; }
+        [Required]
+        [Display(Name = "Area of Specialization")]
+        public string Specialization { get; set; }
+        [Required]
+        [Display(Name = "City of residence")]
+        public string City { get; set; }
+        [Required]
+        [Display(Name = "Province of residence")]
+        public string Province { get; set; }
+        [Required]
+        public string Employer { get; set; }
+
+    }
+}
